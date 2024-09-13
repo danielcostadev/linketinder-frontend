02ff51d3833856da67bbce6c45df2a13ed2242d0
danielcostadev <danielcostamarketing@gmail.com>
2024-09-13 07:07:38 -0300

    Definir classes modelos

--- frontend/src/model/empresa.ts
+++ frontend/src/model/empresa.ts
@@ -1,44 +1,37 @@
 import { Pessoa } from './pessoa';
 
-export class Candidato extends Pessoa {
+export class Empresa extends Pessoa {
 
-    protected cpf: string;
-    protected idade: number;
-    protected formacao: string;
+    protected cnpj: string;
+    protected pais: number;
 
     constructor(
         nome: string,
         email: string,
-        cpf: string,
-        idade: number,
+        cnpj: string,
+        pais: number,
         estado: string,
         cep: string,
         descricao: string,
-        competencias: string[],
-        formacao: string
-
+        competencias: string[]
     ) {
         super(nome, email, estado, cep, descricao, competencias);
-        this.cpf = cpf
-        this.idade = idade
-        this.formacao = formacao
+        this.cnpj = cnpj;
+        this.pais = pais;
     }
-
-    mostrarInformacoes(): string | string[]{
-        return this.competencias,
-               this.formacao
+    mostrarInformacoes(): string | string[] {
+        return this.competencias;
     }
 
     mostrarInformacoesMatch(): string | string[]{
         return this.nome,
                this.email,
-               this.cpf,
-               this.idade,
+               this.cnpj,
+               this.pais,
                this.estado,
                this.cep,
                this.descricao,
                this.competencias
-        
     }
 
 }
